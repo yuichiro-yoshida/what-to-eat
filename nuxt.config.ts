071c395cfe4380f877b4283a7b35868f25760ab6
Yuichiro Yoshida <yuichiro_yoshida@kogasoftware.com>
2022-03-23 00:01:33 +0900

    nuxt.configにbuild設定を追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
+  build: {
+    transpile: ['primevue']
+  },
   css: [
     '~/assets/scss/reset.scss',
     'primevue/resources/themes/saga-blue/theme.css',
