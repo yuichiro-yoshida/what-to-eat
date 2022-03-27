fc8e6686cd520f2d39a11c08a4afef761eee4af1
Yuichiro Yoshida <yuichiro_yoshida@kogasoftware.com>
2022-03-28 00:20:42 +0900

    ミニマムなメタタグ、faviconの設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,4 +21,14 @@ export default defineNuxtConfig({
       },
     },
   },
+  meta: {
+    title: 'What to eat?',
+    meta: [
+      { charset: 'utf-8' },
+      { hid: 'description', name: 'description', content: '妊婦の「これ食べて大丈夫？」「食べた方がいいものは何？」を素早く調べよう' }
+    ],
+    link: [
+      { rel: 'icon', href: '/favicon.ico' }
+    ]
+  }
 })
